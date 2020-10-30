e6136078c7e1285fce853a75dfe7239fba66b9a5
aru99 <mohammadarmanmavric@gmail.com>
2020-10-30 20:57:59 +0530

    added the animation function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // let' do the fun stuff, i.e animations !!!!
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+// moving animation Event
+container.addEventListener('mousemove', (e) => {
+    // console.log(e.pageX, e.pageY);
+    let xAxis = (window.innerWidth /2 - e.pageX) / 25;
+    let yAxis = (window.innerHeight /2 - e.pageY) /25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+
+} );
